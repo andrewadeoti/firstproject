21c644c09ff327433ebde5aa67491e95a35e8f4e
N1148202 <andrew.adeoti2022@my.ntu.ac.uk>
2024-01-17 13:04:31 +0000

    Update User.java

--- JavaFXApplication1/src/main/java/com/mycompany/javafxapplication1/User.java
+++ JavaFXApplication1/src/main/java/com/mycompany/javafxapplication1/User.java
@@ -35,3 +35,15 @@ public class User {
         this.pass.set(pass);
     }
 }
+
+
+
+/*This code is a Java function that defines a User class with two attributes: user and pass. 
+   * The function takes two arguments, user and pass, which are strings that represent the username and password of a user. 
+    *The function creates a new User object with these attributes and returns it to the main program. 
+    *The function also has getter and setter methods for accessing and modifying the user and pass attributes. 
+
+*The function is useful for creating and managing user accounts in a JavaFX application. JavaFX is a framework for creating graphical user interfaces in Java1. 
+ *   The user and pass attributes are stored as SimpleStringProperty objects, which are observable properties that can be bound to other UI elements, such as a TableView2. 
+  *  This allows the UI to automatically update when the user or pass values change. 
+    */
