1d8390afac3fc5b650d695c210c0249d3842f86f
ntu-user <ntu-user@ntu.ac.uk>
2024-02-01 19:47:11 +0000

    Moved the console playground

--- JavaFXApplication1/src/main/java/com/mycompany/javafxapplication1/SecondaryController.java
+++ JavaFXApplication1/src/main/java/com/mycompany/javafxapplication1/SecondaryController.java
@@ -1,5 +1,6 @@
 package com.mycompany.javafxapplication1;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -16,6 +17,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
@@ -23,6 +25,21 @@ import javafx.stage.Stage;
 
 
 public class SecondaryController {
+    
+    ///////Console area playground/////////
+    
+   
+    @FXML
+    private TextField Source;
+    
+    @FXML
+    private TextField Destination;
+    
+    @FXML
+    private TextArea textOutput;
+    
+    ///////Console area playground/////////
+    
     
     @FXML
     private TextField userTextField;
@@ -65,6 +82,117 @@ public class SecondaryController {
             e.printStackTrace();
         }
     }
+    
+    //console functions
+    
+    @FXML
+    private void lsConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.ls("");
+        textOutput.setText("Output: "+output);
+        Source.setText("");
+        Destination.setText("");
+    }
+    
+    @FXML
+    private void treeConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.tree();
+        textOutput.setText("Output: "+output);
+        Source.setText("");
+        Destination.setText("");
+    }
+    
+    
+    //system function
+    @FXML
+    private void whoAmIConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.whoami();
+        textOutput.setText("Output: "+output);
+    }
+    
+    @FXML
+    private void psConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.ps();
+        textOutput.setText("Output: "+output);
+    }
+    
+    
+    
+    //file funcitons
+
+    
+    
+    
+    @FXML
+    private void createFileConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.makeFile(Source.getText());  
+        textOutput.setText("Output: "+output);
+        Destination.setText("");
+    }
+    
+    @FXML
+    private void deleteFileConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.deleteFile(Source.getText());
+        textOutput.setText("Output: "+output);
+        Destination.setText("");
+    }
+    
+    @FXML
+    private void retriveFileConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.retriveFile(Source.getText());
+        textOutput.setText(output);
+        Destination.setText("");
+    }
+    
+    @FXML
+    private void updateFileConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.updateFile(Source.getText(),textOutput.getText());
+        textOutput.setText(output);
+        Destination.setText("");
+    }
+    
+    
+    @FXML
+    private void moveFileConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.mv(Source.getText(),Destination.getText());
+        textOutput.setText("Output: "+output);
+    }
+    
+    @FXML
+    private void copyFileConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.cp(Source.getText(),Destination.getText());
+        textOutput.setText("Output: "+output);
+    }
+    
+    @FXML
+    private void makeFolderConsole(ActionEvent event) throws IOException, InterruptedException
+    {
+        var console = new ConsoleInput();
+        String output = console.mkdir(Source.getText());  
+        textOutput.setText("Output: "+output);
+        Destination.setText("");
+    }
+    
+    
 
     public void initialise(String[] credentials) {
         userTextField.setText(credentials[0]);
